58ec287681d11f9b1b6ca30add547044ce9389d4
kwokpia <kwokpia@gmail.com>
2026-01-28 20:59:48 +0800

    Refactor eBPF/BTF injection and clean up comments

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-# 1. 注入 eBPF/BTF 核心开关到内核模板
-# 这一步是为了让 BTF 符号表出现在 /sys/kernel/btf/vmlinux
+# 1. 注入 eBPF/BTF 到内核模板
 find target/linux/x86 -name "config-*" | xargs -i sh -c "
+    echo 'CONFIG_DEBUG_INFO=y' >> {}
     echo 'CONFIG_DEBUG_INFO_BTF=y' >> {}
     echo 'CONFIG_DEBUG_INFO_BTF_MODULES=y' >> {}
-    echo 'CONFIG_BPF_EVENTS=y' >> {}
     echo 'CONFIG_KPROBES=y' >> {}
     echo 'CONFIG_KPROBE_EVENTS=y' >> {}
+    echo 'CONFIG_BPF_EVENTS=y' >> {}
     echo 'CONFIG_BPF_SYSCALL=y' >> {}
     echo 'CONFIG_BPF_JIT=y' >> {}
-    echo 'CONFIG_DEBUG_INFO=y' >> {}
-    echo 'CONFIG_DEBUG_INFO_DWARF_TOOLCHAIN_DEFAULT=y' >> {}
 "
 
-# 2. 修改默认网关 IP (J4125 建议改为 192.168.1.100 避开冲突)
+# 2. 修改默认网关 IP
 sed -i 's/192.168.1.1/192.168.1.100/g' package/base-files/files/bin/config_generate
 
-# 3. 开启 irqbalance (多核中断负载均衡)
+# 3. 设置 irqbalance 自动启动
 mkdir -p package/base-files/files/etc/uci-defaults
 cat <<EOF > package/base-files/files/etc/uci-defaults/99-custom-settings
 service irqbalance enable
@@ -25,6 +23,6 @@ service irqbalance start
 exit 0
 EOF
 
-# 4. 强力清理残留的无线组件
+# 4. 剔除无线残留
 sed -i '/wpad/d' .config
 sed -i '/hostapd/d' .config
